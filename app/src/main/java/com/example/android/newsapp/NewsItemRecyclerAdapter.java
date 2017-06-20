0b39be40f4317c850d29f112d8017422e2d5db62
simq <simq@lord.com>
2017-06-20 15:15:37 +0300

    Create NewsItemRecyclerAdapter and custom list_item.xml

---
+++ app/src/main/java/com/example/android/newsapp/NewsItemRecyclerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.android.newsapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Adapter for RecyclerView
+ */
+
+public class NewsItemRecyclerAdapter extends RecyclerView.Adapter<NewsItemRecyclerAdapter.ViewHolder> {
+
+    public List<NewsItem> mNewsItems;
+    public MainActivity mContext;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView section;
+        public TextView title;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            section = (TextView) itemView.findViewById(R.id.section);
+            title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+
+    public NewsItemRecyclerAdapter(MainActivity context, List<NewsItem> newsItems){
+        mNewsItems = newsItems;
+        mContext = context;
+    }
+
+    @Override
+    public NewsItemRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View listItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+
+        ViewHolder vh = new ViewHolder(listItem);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(NewsItemRecyclerAdapter.ViewHolder holder, int position) {
+        final NewsItem currentNewsItem = mNewsItems.get(position);
+
+        holder.section.setText(currentNewsItem.getSection());
+        holder.title.setText(currentNewsItem.getTitle());
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return mNewsItems.size();
+    }
+}
