9b108974bd5b5be34dfcab9516dacea91ff6eb4f
simq <simq@lord.com>
2017-06-20 21:39:54 +0300

    Create NewsItemLoader

--- app/src/main/java/com/example/android/newsapp/NewsItemLoader.java
+++ app/src/main/java/com/example/android/newsapp/NewsItemLoader.java
@@ -1,8 +1,56 @@
 package com.example.android.newsapp;
 
-/**
- * Created by tsare on 20.06.2017.
- */
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
 
-public class NewsItemLoader {
+import java.util.List;
+
+/**
+ * Loads a list of books by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class NewsItemLoader extends AsyncTaskLoader<List<NewsItem>> {
+
+    /**
+     * Tag for log messages
+     */
+    private static final String LOG_TAG = NewsItemLoader.class.getName();
+
+    /**
+     * Query URL
+     */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link NewsItemLoader}.
+     *
+     * @param context of the activity
+     * @param url     to load the data from
+     */
+    public NewsItemLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<NewsItem> loadInBackground() {
+
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response and extract a list of earthquakes.
+        List<NewsItem> newsItems = QueryUtils.fetchNewsData(mUrl);
+        return newsItems;
+    }
 }
