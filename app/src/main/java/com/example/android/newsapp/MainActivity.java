6d1d89957abf13a245b434a4d8df4d14088fe6d0
simq <simq@lord.com>
2017-06-22 09:53:38 +0300

    Implement Loader and ConnectivityManager

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,15 +1,111 @@
 package com.example.android.newsapp;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsItem>> {
 
     public static final String LOG_TAG = MainActivity.class.getName();
+
+    /**
+     *URL for news items data from the Guardian website
+     */
+    private static final String GUARDIAN_REQUEST_URL ="https://content.guardianapis.com/search?q=estonia&api-key=test";
+
+    /**
+     * TextView that is displayed when the list is empty
+     */
+    private TextView mEmptyStateTextView;
+
+    private RecyclerView mRecyclerView;
+    private NewsItemRecyclerAdapter mNewsItemRecyclerAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    /**
+     * Constant value for the news item loader ID. Can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int NEWSITEM_LOADER_ID = 1;
+
+    @Override
+    public Loader<List<NewsItem>> onCreateLoader(int i, Bundle bundle) {
+
+        // Create a new loader for the given URL
+        return new NewsItemLoader(this, GUARDIAN_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<NewsItem>> loader, List<NewsItem> newsItems) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mNewsItemRecyclerAdapter = new NewsItemRecyclerAdapter(MainActivity.this, new ArrayList<NewsItem>());
+
+        if (newsItems != null && !newsItems.isEmpty()) {
+            mNewsItemRecyclerAdapter = new NewsItemRecyclerAdapter(MainActivity.this, newsItems);
+            mRecyclerView.setAdapter(mNewsItemRecyclerAdapter);
+        } else {
+            // Set empty state text to display "No news found"
+            mEmptyStateTextView.setText("No earthquakes found");
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<NewsItem>> loader) {
+
+        mNewsItemRecyclerAdapter = new NewsItemRecyclerAdapter(MainActivity.this, new ArrayList<NewsItem>());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data.
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            Log.i(LOG_TAG, "TEST: calling initLoader() ...");
+            loaderManager.initLoader(NEWSITEM_LOADER_ID, null, this);
+        } else {
+            // Otherwise display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText("No internet connection");
+        }
+
     }
 }
